6efe8ea3c6e95c806bab4ef7da65817e890d1625
Roy Buitenhuis <roy.buitenhuis94@gmail.com>
2014-03-27 13:57:05 +0100

    Cleaned up tab's, white spaces and cleaned the mess of curly braces up.

--- NFC_v6.ino
+++ NFC_v6.ino
@@ -3,20 +3,22 @@
 #include <SPI.h>
 #include <Wire.h>
 #include <Ethernet.h>
+
+#define PN532_CS 9
+#define SCK  52  
+#define MOSI 51  
+#define MISO 50
+#define SS 9
+//if debugging, uncomment below, else comment below
+#define  NFC_DEMO_DEBUG 1
+
+
 const int chipSelect = 53;
 int buzzer = 3;
 int start = 4;
-	#define PN532_CS 9
-	#define SCK  52  
-	#define MOSI 51  
-	#define MISO 50
-	#define SS 9
-
 File dataWrite;
 File dataRead;
 PN532 nfc(PN532_CS);
-#define  NFC_DEMO_DEBUG 1
-
 char buffer[50];
 char Geheugen[100];
 byte server[] = { 131,180,122,144 }; 
@@ -29,46 +31,40 @@ int stringPos = 0;
 boolean startRead; 
 
 
-
 void setup(void) {
-     Ethernet.begin(mac, ip);
+  Ethernet.begin(mac, ip);
   pinMode(buzzer, OUTPUT);
   pinMode(53, OUTPUT);
   pinMode(4, OUTPUT);
-   Serial.begin(9600);
-   Serial.println("Bradley But");
-   Serial.println("No arduinos were euthanised during development of this product");
-   SPI.setDataMode(SPI_MODE1);
-   
-   
-      #ifdef NFC_DEMO_DEBUG
-      Serial.print("Initializing SD card...");
-      analogWrite(buzzer, 20);
-      delay(70);
-      analogWrite(buzzer, 0);
-      delay(30);
-      analogWrite(buzzer, 20);
-      delay(70);
-      analogWrite(buzzer, 0);
-      delay(30);
-      analogWrite(buzzer, 20);
-      delay(70);
-      analogWrite(buzzer, 0);
-      
-      
-        #endif
-        pinMode(10, OUTPUT);
-        
-            if (!SD.begin(4)) {
-            #ifdef NFC_DEMO_DEBUG
-            Serial.println("Card failed, or not present");
-
-              #endif
-              return;
-              }
+  Serial.begin(9600);
+  Serial.println("Bradley But");
+  Serial.println("No arduinos were euthanised during development of this product");
+  SPI.setDataMode(SPI_MODE1);
+    
+#ifdef NFC_DEMO_DEBUG
+  Serial.print("Initializing SD card...");
+  analogWrite(buzzer, 20);
+  delay(70);
+  analogWrite(buzzer, 0);
+  delay(30);
+  analogWrite(buzzer, 20);
+  delay(70);
+  analogWrite(buzzer, 0);
+  delay(30);
+  analogWrite(buzzer, 20);
+  delay(70);
+  analogWrite(buzzer, 0);
+#endif
+  pinMode(10, OUTPUT);        
+  if (!SD.begin(4)) {
+  #ifdef NFC_DEMO_DEBUG
+    Serial.println("Card failed, or not present");
+  #endif
+    return;
+  }
   Serial.println("card initialized.");
   //++++++++++++++++++++++++++++++++++++++++++++++-- SD bestand openen--++++++
-   dataWrite = SD.open("datalog.txt", FILE_WRITE);
+  dataWrite = SD.open("datalog.txt", FILE_WRITE);
   
   // if the file opened okay, write to it:
   if (dataWrite)
@@ -77,20 +73,16 @@ void setup(void) {
     dataWrite.println("Reboot");
 	// close the file:
     dataWrite.close();
-    Serial.println("done.");
-    
-    
-          } else {
-           // if the file didn't open, print an error:
-           Serial.println("error opening test.txt");
-           //++++++++++++++++++++++++++++++++++++++++++++++-- SD bestand openen--++++++
+    Serial.println("done."); 
+  } else {
+    // if the file didn't open, print an error:
+    Serial.println("error opening test.txt");
+    //++++++++++++++++++++++++++++++++++++++++++++++-- SD bestand openen--++++++
   }
-   
 #ifdef NFC_DEMO_DEBUG
   Serial.println("Hello!");
 #endif
   nfc.begin();
-
   uint32_t versiondata = nfc.getFirmwareVersion();
 #ifdef NFC_DEMO_DEBUG  
   if (! versiondata)
@@ -109,100 +101,80 @@ void setup(void) {
 #endif
   nfc.SAMConfig();
   dataWrite = SD.open("datalog.txt", FILE_WRITE);
-  if(!dataWrite)
 #ifdef NFC_DEMO_DEBUG
+  if(!dataWrite)
     Serial.println("Couldn't open datafile on sd card"); 
 #endif
 }
 
 void loop(void) {
-    nfc.begin();
-    SD.begin(4);
+  nfc.begin();
+  SD.begin(4);
   uint32_t id;
   //++++++++++++++++++++++++++++++++++--zoeken naar de nfc--++++++++++++++++++++++
   id = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A);
-     dataWrite = SD.open("datalog.txt", FILE_WRITE);
-     Geheugen = SD.open("datalog.txt", FILE_READ);
-     
+  dataWrite = SD.open("datalog.txt", FILE_WRITE);
+  Geheugen = SD.open("datalog.txt", FILE_READ);
 
-
-    if (id != 0 && id == Geheugen)
-    
+  if (id != 0 && id == Geheugen)
     digitalWrite(start, HIGH);
-    }
-    
-    else if ( id != 0 && != Geheugen)
-    {
-      Serial.println("Updating");
-      analogWrite(buzzer, 20);
-      delay(70);
-      analogWrite(buzzer, 0);
-      delay(30);
-      analogWrite(buzzer, 20);
-      delay(70);
-      analogWrite(buzzer, 0);
-      delay(30);
-      analogWrite(buzzer, 20);
-      delay(70);
-      analogWrite(buzzer, 0);
-      
-        String pageValue = connectAndRead(); 
-        Serial.println(pageValue); 
-        delay(5000); 
-
-          String connectAndRead(){
-      
-            Serial.println("connecting...");
+  else if ( id != 0 && != Geheugen)
+  {
+    Serial.println("Updating");
+    analogWrite(buzzer, 20);
+    delay(70);
+    analogWrite(buzzer, 0);
+    delay(30);
+    analogWrite(buzzer, 20);
+    delay(70);
+    analogWrite(buzzer, 0);
+    delay(30);
+    analogWrite(buzzer, 20);
+    delay(70);
+    analogWrite(buzzer, 0);  
+    String pageValue = connectAndRead(); 
+    Serial.println(pageValue); 
+    delay(5000); 
+  }
+}
+String connectAndRead(){
+  Serial.println("connecting...");
   if (client.connect(server, 80)) {
     Serial.println("connected");
     client.print("GET ");
     client.println(location);
     client.println();
     return readPage();
-
   }else{
     return "connection failed";
   }
-
 }
-
 String readPage(){
-
-
   stringPos = 0;
   memset( &inString, 0, 4096 ); 
-
   while(true){
-
     if (client.available()) {
       char c = client.read();
-
       if (c == '[' ) { 
         startRead = true;
-       
       }
       else if(startRead)
       {
-          if(c != ']')
-          {
-          inString[stringPos] = c;
-          //stringPos ++;
-          Serial.print(inString);
-          }
-            else
-            {
-        
+        if(c != ']')
+        {
+        inString[stringPos] = c;
+        //stringPos ++;
+        Serial.print(inString);
+        }
+        else
+        {
           client.flush();
           Serial.println("disconnecting.");
           return inString;
-
         }
-
       }
     }
-
   }
-
 }
 
     
